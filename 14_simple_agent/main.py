15c2d4482a4733fdbed09561e7110f91b572afe3
arsalan4work <m.arsalan4210@gmail.com>
2025-03-22 18:04:34 +0500

    add files

---
+++ 14_simple_agent/main.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from agents import Agent, Runner, OpenAIChatCompletionsModel, AsyncOpenAI
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=provider
+)
+
+agent = Agent(
+    name="Greeting Agent",
+    instructions="""You are a Greeting Agent, Your task is to greet the user with a friendly message, 
+    when someone says hi you've reply back with salam from Arsalan, if someone says bye then say 
+    allah hafiz from Arsalan, when someone asks other than greeting then say Arsalan is here 
+    just for greeting, I can't answer anything else, sorry.""",
+    model=model
+)
+
+user_question = input("\nEnter your question: ")
+
+result = Runner.run_sync(agent, user_question)
+print(result.final_output)
